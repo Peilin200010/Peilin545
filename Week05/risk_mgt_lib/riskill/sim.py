b011aca0a722eae7eaad8368f075609548529a96
pl212 <pl212@duke.edu>
2023-02-28 01:23:28 -0500

    add right code

---
+++ Week05/risk_mgt_lib/riskill/sim.py
@@ -0,0 +1,55 @@
+import numpy as np
+import math
+
+
+class Direct:
+    @staticmethod
+    def chol_psd(mat: np.ndarray) -> np.ndarray:
+        number = mat.shape[0]
+        root = np.zeros((number, number))
+        for j in range(number):
+            s = 0.
+            if j > 0:
+                s = root[j, :j]@root[j, :j].T
+            temp = mat[j, j] - s
+            if -1e-8 <= temp <= 0:
+                temp = 0.
+            root[j, j] = math.sqrt(temp)
+
+            if root[j, j] == 0:
+                root[j:, j] = 0.
+            else:
+                for i in range(j+1, number):
+                    s = root[i, :j]@root[j, :j].T
+                    root[i, j] = (mat[i, j] - s) / root[j, j]
+        return root
+
+    def simulation(self, mat: np.ndarray, draw: int = 25000) -> np.ndarray:
+        L = self.chol_psd(mat)
+        Z = np.random.standard_normal((L.shape[0], draw))
+        X = (L @ Z).T
+        return X
+
+
+class PCA:
+    @staticmethod
+    def conduct_pca(mat: np.ndarray) -> (np.ndarray, np.ndarray, list):
+        eigenvalues, eigenvectors = np.linalg.eigh(mat)
+        e_vals_sort = np.flip(np.real(eigenvalues), axis=0)
+        pos_e_vals = e_vals_sort[e_vals_sort >= 1e-8]
+        pos_e_vecs = np.flip(np.real(eigenvectors), axis=1)[:, : pos_e_vals.size]
+        cum_explain = [pos_e_vals[:i+1].sum() / pos_e_vals.sum() for i in range(pos_e_vals.size)]
+        return pos_e_vals, pos_e_vecs, cum_explain
+
+    def simulation(self, mat: np.ndarray, draw: int = 25000, min_explain: float = 1.0):
+        pca = self.conduct_pca(mat)
+        pca_vals = pca[0]
+        pca_vecs = pca[1]
+        for i, cum in enumerate(pca[2]):
+            if cum >= min_explain:
+                pca_vals = pca_vals[: i + 1]
+                pca_vecs = pca_vecs[:, : i + 1]
+        B = pca_vecs @ np.diag(np.sqrt(pca_vals))
+        r = np.random.standard_normal((B.shape[1], draw))
+        X = (B @ r).T
+        return X
